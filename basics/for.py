10c60f99fbc40b1c00585cb6e681cb39086299e4
Jurijs Kolomijecs <jurijs.kolomijecs@tele2.com>
2020-08-30 11:11:55 +0300

    For loop basics

---
+++ basics/for.py
@@ -0,0 +1,11 @@
+point_3d = (1.0, 2.0, 3.0)
+for point in point_3d:
+    print(f"Point: {point}")
+
+list_of_points = [(1.0, 2.0), (3.0, 4.0), (5.0, 6.0)]
+for x, y in list_of_points:
+    print(f"X: {x}, Y: {y}")
+
+ages = { 'Name1' : 11, 'Name2' : 22, 'Name3' : 33 }
+for name, age in ages.items():
+    print(f"{name} has age of {age}")
