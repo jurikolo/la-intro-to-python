c2df48e92b0988cab6ae08f1b3006de1fc1f58d1
Jurijs Kolomijecs <jurijs.kolomijecs@tele2.com>
2020-09-03 13:59:58 +0300

    Working with standard Python packages

---
+++ packages/standard.py
@@ -0,0 +1,11 @@
+print("Modules documentation: https://docs.python.org/3/tutorial/modules.html")
+print("Standard modules list: https://docs.python.org/3/py-modindex.html")
+
+import math
+print(math.pi)
+
+from math import pi
+print(pi)
+
+from math import pi as p
+print(p)
