658bb16a9c4163a3b2917d1bb0b9d1881ef7bcda
Jurijs Kolomijecs <jurijs.kolomijecs@tele2.com>
2020-09-11 15:48:47 +0300

    Initial notes web app

---
+++ projects/notes/__init__.py
@@ -0,0 +1,27 @@
+import os
+import functools
+
+from flask import Flask, render_template
+from flask_migrate import Migrate
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY=os.environ.get('SECRET_KEY', default='dev')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    from .models import db
+
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    @app.route('/sign_up')
+    def sign_up():
+        return render_template('sign_up.html')
+
+    return app
